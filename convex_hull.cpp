8317bd61f3163dc030e2127003167a44ee054823
zeus-iitg <42604439+zeus-iitg@users.noreply.github.com>
2019-10-04 15:14:33 +0530

    Add files via upload

---
+++ convex_hull.cpp
@@ -0,0 +1,101 @@
+/*Author  : Vaibhav Kumar Singh
+  Roll No.: 180101086*/
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/__gcd((a),(b))
+#define clr(x) x.clear()
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define mp make_pair
+#define hell 1000000007
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define all(a) a.begin(),a.end()
+
+#define o_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define o_setll tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+using namespace std;
+using namespace __gnu_pbds;
+
+struct pt {
+    int x, y;
+};
+
+bool isEqual(pt a,pt b){
+    if(a.x==b.x&&a.y==b.y)
+        return true;
+    return false;
+}
+
+bool cmp(pt a, pt b) {
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
+}
+
+bool cw(pt a, pt b, pt c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
+}
+
+bool ccw(pt a, pt b, pt c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
+}
+
+void convex_hull(vector<pt>& a) {
+    if (a.size() == 1)
+        return;
+
+    sort(a.begin(), a.end(), &cmp);
+    pt p1 = a[0], p2 = a.back();
+    vector<pt> up, down;
+    up.push_back(p1);
+    down.push_back(p1);
+    for (int i = 1; i < (int)a.size(); i++) {
+        if (i == a.size() - 1 || cw(p1, a[i], p2)) {
+            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
+                up.pop_back();
+            up.push_back(a[i]);
+        }
+        if (i == a.size() - 1 || ccw(p1, a[i], p2)) {
+            while(down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
+                down.pop_back();
+            down.push_back(a[i]);
+        }
+    }
+
+    a.clear();
+    for (int i = 0; i <up.size(); i++)
+        a.push_back(up[i]);
+    for (int i = down.size()-2; i >0; i--)
+        a.push_back(down[i]);
+}
+
+int main()
+{
+    IOS
+    int n;
+    cin>>n;
+    vector<pt> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i].x>>v[i].y;
+    convex_hull(v);
+    cout<<v.size()<<"\n";
+    for(int i=0;i<v.size();i++)
+        cout<<v[i].x<<" "<<v[i].y<<"\n";
+    return 0;
+}
