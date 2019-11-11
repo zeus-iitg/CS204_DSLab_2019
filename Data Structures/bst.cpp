2f6d0b303d4be8db5b1384f900afcfa375486491
zeus-iitg <42604439+zeus-iitg@users.noreply.github.com>
2019-11-11 15:26:32 +0530

    Create bst.cpp

---
+++ Data Structures/bst.cpp
@@ -0,0 +1,168 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/__gcd((a),(b))
+#define clr(x) x.clear()
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define mp make_pair
+#define hell 1000000007
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define all(a) a.begin(),a.end()
+#define ign cin.ignore();
+
+#define o_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define o_setll tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+struct node
+{
+	int key;
+	struct node *left, *right;
+};
+
+struct node *newNode(int item)
+{
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->key = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+void inorder(struct node *root)
+{
+	if (root != NULL)
+	{
+		inorder(root->left);
+		printf("%d ", root->key);
+		inorder(root->right);
+	}
+}
+
+struct node* insert(struct node* node, int key)
+{
+
+	if (node == NULL) return newNode(key);
+	if (key < node->key)
+		node->left = insert(node->left, key);
+	else
+		node->right = insert(node->right, key);
+	return node;
+}
+
+struct node * minValueNode(struct node* node)
+{
+	struct node* current = node;
+	while (current && current->left != NULL)
+		current = current->left;
+
+	return current;
+}
+
+struct node* deleteNode(struct node* root, int key)
+{
+	// base case
+	if (root == NULL) return root;
+	if (key < root->key)
+		root->left = deleteNode(root->left, key);
+	else if (key > root->key)
+		root->right = deleteNode(root->right, key);
+	else
+	{
+
+		if (root->left == NULL)
+		{
+			struct node *temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL)
+		{
+			struct node *temp = root->left;
+			free(root);
+			return temp;
+		}
+		struct node* temp = minValueNode(root->right);
+		root->key = temp->key;
+		root->right = deleteNode(root->right, temp->key);
+	}
+	return root;
+}
+
+int main()
+{
+	IOS
+	struct node *root = NULL;
+	root = insert(root, 50);
+	root = insert(root, 30);
+	root = insert(root, 20);
+	root = insert(root, 40);
+	root = insert(root, 70);
+	root = insert(root, 60);
+	root = insert(root, 80);
+
+	printf("Inorder traversal of the given tree \n");
+	inorder(root);
+
+	printf("\nDelete 20\n");
+	root = deleteNode(root, 20);
+	printf("Inorder traversal of the modified tree \n");
+	inorder(root);
+
+	printf("\nDelete 30\n");
+	root = deleteNode(root, 30);
+	printf("Inorder traversal of the modified tree \n");
+	inorder(root);
+
+	printf("\nDelete 50\n");
+	root = deleteNode(root, 50);
+	printf("Inorder traversal of the modified tree \n");
+	inorder(root);
+
+	return 0;
+}
