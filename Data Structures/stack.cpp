e5ca5ad2d802d39197374f125cb8c320f82a5e23
zeus-iitg <42604439+zeus-iitg@users.noreply.github.com>
2019-11-11 15:01:23 +0530

    Create stack.cpp

---
+++ Data Structures/stack.cpp
@@ -0,0 +1,102 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/__gcd((a),(b))
+#define clr(x) x.clear()
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define mp make_pair
+#define hell 1000000007
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define all(a) a.begin(),a.end()
+#define ign cin.ignore();
+
+#define o_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define o_setll tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+class STACK
+{
+    private:
+    int arr[100000];
+    int len=0;
+public:
+    int size(){
+        return len;
+    }
+    int top(){
+        if(len!=0)return arr[len-1];
+        return -1;
+    }
+    void push(int x){
+        len++;
+        arr[len-1]=x;
+    }
+    void pop(){
+        if(len==0)return;
+        len--;
+    }
+};
+
+int main()
+{
+    IOS
+    STACK s;
+    s.push(1);
+    cout<<s.size()<<"\n";
+    cout<<s.top()<<"\n";
+    s.push(2);
+    s.push(3);
+    cout<<s.size()<<"\n";
+    s.push(5);
+    s.pop();
+    cout<<s.size()<<"\n";
+    s.pop();
+    cout<<s.top()<<"\n";
+    return 0;
+}
